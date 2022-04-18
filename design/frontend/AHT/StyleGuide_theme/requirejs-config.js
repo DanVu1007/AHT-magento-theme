696c8a05b81b0653f78ed3508d6e038b5fb017d2
langoclinh2312 <vnbboy12345@gmail.com>
2022-04-18 09:05:56 +0700

    css style-duide

--- design/frontend/AHT/StyleGuide_theme/requirejs-config.js
+++ design/frontend/AHT/StyleGuide_theme/requirejs-config.js
@@ -1,10 +1,22 @@
 var config = {
+    map: {
+        '*': {
+            'scriptName':'AHT_StyleGuide_theme/js/scriptname'
+        }
+    },
+    config: {
+        mixins: {
+            'Magento_Checkout/js/model/shipping-save-processor/payload-extender': {
+                'AHT_StyleGuide_theme/js/model/shipping-save-processor/payload-extender-mixin': true
+            }
+        }
+    },
     paths: {
-        slick:        'js/slick.min'
+        slick: 'js/slick/slick.min'
     },
     shim: {
         slick: {
             deps: ['jquery']
         }
-    }
+      }
 };
