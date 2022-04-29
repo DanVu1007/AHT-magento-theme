84feabaf90f87fad6dc583d3fed00a4b44395f49
langoclinh2312 <vnbboy12345@gmail.com>
2022-04-29 14:54:16 +0700

    footer

--- design/frontend/AHT/StyleGuide_theme/requirejs-config.js
+++ design/frontend/AHT/StyleGuide_theme/requirejs-config.js
@@ -1,16 +1,4 @@
 var config = {
-    map: {
-        '*': {
-            'scriptName': 'AHT_StyleGuide_theme/js/scriptname'
-        }
-    },
-    config: {
-        mixins: {
-            'Magento_Checkout/js/model/shipping-save-processor/payload-extender': {
-                'AHT_StyleGuide_theme/js/model/shipping-save-processor/payload-extender-mixin': true
-            }
-        }
-    },
     paths: {
         slick: 'js/slick.min'
     },
